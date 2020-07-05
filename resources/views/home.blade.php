4b3d3f8d1f05a37f118e5807a69b855aae10bd0e
armendzz <armendzekjiri@gmail.com>
2020-07-05 17:07:15 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <title>Giardino GartenBau</title>
+</head>
+<body>
+    <div class="container">
+    <header>
+<!--Main Navigation-->
+<header>
+
+    <nav class="navbar navbar-expand-lg navbar-dark font-weight-bold green">
+      <a class="navbar-brand" href="#"><strong>Giardino GartenBau</strong></a>
+   
+      <ul class="navbar-nav mr-auto">
+     
+      </ul>
+    <div class="float-right">
+      <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item ">
+            <i class="fas fa-phone-volume"></i> <strong> <a class="nav-item" href="tel:017630720324">017630720324</a></strong>
+          </li>
+        </ul>
+    </div>
+    </nav>
+  
+  </header>
+  <!--Main Navigation-->
+        <!-- Jumbotron -->
+<div class="card card-image" style="background-image: url(https://www.bolliger-gartenbau.ch/fileadmin/img/page-1-promo-image.jpg);">
+    <div class="text-white text-center rgba-stylish-strong py-5 px-4">
+      <div class="py-5">
+  
+        <!-- Content -->
+        
+        <h2 class="card-title h2 my-4 py-2">Wir kümmern uns um Ihren Garten - und Sie genießen!</h2>
+        <p class="mb-4 pb-2 px-md-5 mx-md-5">Unser Team bringt Ihr privates und öffentliches Grün in Topform. Mit außergewöhnlichen Gestaltungsideen, professioneller Planung und liebevoller Ausführung bekommen Sie bei uns alles rund um Natur und Garten.</p>
+        <a href="tel:017630720324" class="btn btn-dark-green"><i class="fas fa-phone-volume"></i> 017630720324</a> 
+  
+      </div>
+    </div>
+  </div>
+  <!-- Jumbotron -->
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark green">
+           
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span> Menu
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link " href="#"><strong>Home </strong><span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"> <strong>Leistungen</strong> </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"> <strong>Garten Projekte</strong> </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> <strong>Die Gartenschönmacher</strong> </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"> <strong>Galerie</strong> </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"> <strong>Kontakt</strong> </a>
+                  </li>
+              </ul>
+             
+            </div>
+          </nav>
+    </header>
+
+    <main class="mt-2" >
+
+
+       
+
+        
+        <!-- Card group -->
+<div class="card-group mt-2">
+
+    <!-- Card -->
+    <div class="card mb-4 mr-2">
+  
+      <!-- Card image -->
+      <div class="view overlay">
+        <img class="card-img-top" height="220px" src="https://images.squarespace-cdn.com/content/v1/56d252832b8dde6611c90bac/1458541068052-H0UJ8GEH03LIB3358FYB/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFUQAah1E2d0qOFNma4CJuw0VgyloEfPuSsyFRoaaKT76QvevUbj177dmcMs1F0H-0/image-asset.png"
+          alt="Card image cap">
+        <a href="#!">
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+  
+      <!-- Card content -->
+      <div class="card-body">
+  
+        <!-- Title -->
+        <h4 class="card-title">Beratung</h4>
+        <!-- Text -->
+      
+        <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+       
+  
+      </div>
+      <!-- Card content -->
+  
+    </div>
+    <!-- Card -->
+  
+    <!-- Card -->
+    <div class="card mb-4 mr-2">
+  
+      <!-- Card image -->
+      <div class="view overlay">
+        <img class="card-img-top" height="220px" src="https://www.fantasticgardenersmelbourne.com.au/wp-content/uploads/2017/06/garden-plan-1024x576.png"
+          alt="Card image cap">
+        <a href="#!">
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+  
+      <!-- Card content -->
+      <div class="card-body">
+        <!-- Title -->
+        <h4 class="card-title">Planung</h4>
+        <!-- Text -->
+      
+        <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+       
+  
+      </div>
+      <!-- Card content -->
+  
+    </div>
+    <!-- Card -->
+  
+    <!-- Card -->
+    <div class="card mb-4 mr-2">
+  
+      <!-- Card image -->
+      <div class="view overlay">
+        <img class="card-img-top " height="220px" src="https://gardenandhappy.com/wp-content/uploads/2019/02/Summer-Vegetable-Garden-Layout-800x483.png"
+          alt="Card image cap">
+        <a href="#!">
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+  
+      <!-- Card content -->
+      <div class="card-body">
+  
+        <!-- Title -->
+        <h4 class="card-title">Gestaltung</h4>
+        <!-- Text -->
+        
+        <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+       
+  
+      </div>
+      <!-- Card content -->
+  
+    </div>
+    <!-- Card -->
+    <div class="card mb-4">
+  
+        <!-- Card image -->
+        <div class="view overlay">
+          <img class="card-img-top " height="220px" src="https://webstockreview.net/images/clipart-grass-cut-out.png"
+            alt="Card image cap">
+          <a href="#!">
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+    
+        <!-- Card content -->
+        <div class="card-body">
+    
+          <!-- Title -->
+          <h4 class="card-title">Pflege</h4>
+          <!-- Text -->
+          
+          <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+         
+    
+        </div>
+        <!-- Card content -->
+    
+      </div>
+      <!-- Card -->
+  
+      <div class="row pr-lg-5">
+        <div class="col-md-7 mb-4">
+  
+          <div class="view">
+            <img src="https://www.dlf.pt/png/big/23/234508_garden-png.png" class="img-fluid" alt="smaple image">
+          </div>
+  
+        </div>
+        <div class="col-md-5 d-flex align-items-center">
+          <div>
+            
+            <h3 class="font-weight-bold mb-4">UNSERE PHILOSOPHIE</h3>
+            <h4> Wir möchten, dass Sie sich in Ihrem Garten wohlfühlen! </h4>
+              <p>Zufriedene Kunden - das ist unser höchstes Ziel. Wir führen unser Geschäft nach alten traditionsreichen Tugenden und christlichen Werten und das sind unsere Geschäftsprinzipien: Offene und ehrliche Beratung, fachlich kompetente Arbeitsweise, faire Kalkulation, zuverlässige Terminabsprachen und transparente, offizielle Abrechnungen. Vielleicht sehen wir uns schon bald in Ihrem Garten zu einem persönlichen Beratungsgespräch.</p>
+  
+              <button type="button" class="btn btn-success btn-rounded mx-0">Kontakt</button>
+  
+          </div>
+        </div>
+      </div>
+
+
+
+  </div>
+<!--Google map-->
+
+
+  <div class="container my-5 py-5 z-depth-1">
+
+ 
+    <!--Section: Content-->
+    <section class="px-md-5 mx-md-5 dark-grey-text text-center text-lg-left">
+
+      <!--Grid row-->
+      <div class="row">
+
+        <!--Grid column-->
+        <div class="col-lg-6 mb-4 mb-lg-0 d-flex justify-content-center">
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2478.917741952951!2d6.835403415608014!3d51.528813916958505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b8eb287c6d9acb%3A0xf8488646e20efe12!2sF%C3%B6rsterstra%C3%9Fe%2046%2C%2046149%20Oberhausen%2C%20Germany!5e1!3m2!1sen!2sus!4v1593959477506!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-6 mb-4 mb-lg-0">
+
+          <h3 class="font-weight-bold">Hier besuchen Sie uns:</h3>
+
+          <p class="font-weight-bold">Giardino GartenBau</p>
+        
+          <p>  Addresse: <strong> Försterstraße 46 - 46045 Oberhausen</strong></p>
+          <p>  Email: <strong>giardinogartenbau@gmail.com</strong> </p>
+          <p>  Handy: <strong> <a href="tel:017630720324">017630720324</a> </strong>  </p>
+          <a class="font-weight-bold" href="#" >ÜBER UNS<i class="fas fa-angle-right ml-2"></i></a>
+
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+
+
+    </section>
+    <!--Section: Content-->
+
+
+  </div>
+
+  <!-- Card group -->
+    </main>
+
+   <!-- Footer -->
+<footer class="page-footer font-small green pt-4">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+  
+      <!-- Grid row -->
+      <div class="row">
+  
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+  
+          <!-- Content -->
+          <h5 class="text-uppercase">Bürozeiten</h5>
+          <p>Mo - Sa:  &nbsp;&nbsp; &nbsp;&nbsp;  7:00 - 17:00</p>
+          <p>So:  &nbsp;&nbsp;  &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;    Geschlossen</p>
+  
+        </div>
+        <!-- Grid column -->
+  
+        <hr class="clearfix w-100 d-md-none pb-3">
+  
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+  
+          <!-- Links -->
+          <h5 class="text-uppercase">Links</h5>
+  
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">Facebook</a>
+            </li>
+            <li>
+              <a href="#!">Instagram</a>
+            </li>
+            <li>
+              <a href="#!">Kunden seite</a>
+            </li>
+            <li>
+              <a href="#!">Bewertungen</a>
+            </li>
+          </ul>
+  
+        </div>
+        <!-- Grid column -->
+  
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+  
+          <!-- Links -->
+          <h5 class="text-uppercase">Links</h5>
+  
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">Projekten</a>
+            </li>
+            <li>
+              <a href="#!">Leistungen</a>
+            </li>
+            <li>
+              <a href="#!">Datenschutz</a>
+            </li>
+            <li>
+              <a href="#!">Impressum</a>
+            </li>
+          </ul>
+  
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Grid row -->
+  
+    </div>
+    <!-- Footer Links -->
+  
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="https://giardinogartenbau.com/"> GiardinoGartenBau.com</a>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+</div>
+  <!-- Footer -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js">
+    </script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
+    </script>
+</body>
+
+</html>
